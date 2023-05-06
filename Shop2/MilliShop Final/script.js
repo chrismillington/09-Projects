15cbfd3d5003cf8aa050be36436e5b76f27486aa
Chris Millington <chris@themadllama.com>
2023-05-06 16:58:27 +0100

    View Items 0.01

--- Shop2/MilliShop Final/script.js
+++ Shop2/MilliShop Final/script.js
@@ -35,14 +35,14 @@ function addStock(itemName, itemDescription, itemPicture, itemPrice) {
     }
     )
 }
-
+   
 function start() { 
-    addStock("Car", "Twin exhaust = Wheelbarrow","https://cdn.imagin.studio/getImage?&customer=carwow&tailoring=carwow&make=volvo&modelFamily=xc40&modelRange=recharge&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0188&fileType=png&zoomType=fullscreen&width=800&angle=23&billingTag=web",5275)
-    addStock("Skates","Penguin sized","https://cdn.shopify.com/s/files/1/0331/9201/8057/products/graf-500-figure-ice-skates-white-figure-skates-29164679233685_600x.jpg?v=1647325087",17.99 )
-    addStock("Bike", "Big and Blue", "https://i.shgcdn.com/fe14ddba-4af4-4e0f-aa6c-dfc91890cef2/-/format/auto/-/preview/3000x3000/-/quality/lighter/", 170.99);
-    addStock("Car", "Twin exhaust = Wheelbarrow","https://cdn.imagin.studio/getImage?&customer=carwow&tailoring=carwow&make=volvo&modelFamily=xc40&modelRange=recharge&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0188&fileType=png&zoomType=fullscreen&width=800&angle=23&billingTag=web",5275)
-    addStock("Skates","Penguin sized","https://cdn.shopify.com/s/files/1/0331/9201/8057/products/graf-500-figure-ice-skates-white-figure-skates-29164679233685_600x.jpg?v=1647325087",17.99 )
-    addStock("Bike", "Big and Blue", "https://i.shgcdn.com/fe14ddba-4af4-4e0f-aa6c-dfc91890cef2/-/format/auto/-/preview/3000x3000/-/quality/lighter/", 170.99);
+    addStock("Car", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.","https://cdn.imagin.studio/getImage?&customer=carwow&tailoring=carwow&make=volvo&modelFamily=xc40&modelRange=recharge&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0188&fileType=png&zoomType=fullscreen&width=800&angle=23&billingTag=web",5275)
+    addStock("Skates","Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.","https://cdn.shopify.com/s/files/1/0331/9201/8057/products/graf-500-figure-ice-skates-white-figure-skates-29164679233685_600x.jpg?v=1647325087",17.99 )
+    addStock("Bike", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.", "https://i.shgcdn.com/fe14ddba-4af4-4e0f-aa6c-dfc91890cef2/-/format/auto/-/preview/3000x3000/-/quality/lighter/", 170.99);
+    addStock("Car", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.","https://cdn.imagin.studio/getImage?&customer=carwow&tailoring=carwow&make=volvo&modelFamily=xc40&modelRange=recharge&modelVariant=od&modelYear=2020&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=pspc0188&fileType=png&zoomType=fullscreen&width=800&angle=23&billingTag=web",5275)
+    addStock("Skates","Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.","https://cdn.shopify.com/s/files/1/0331/9201/8057/products/graf-500-figure-ice-skates-white-figure-skates-29164679233685_600x.jpg?v=1647325087",17.99 )
+    addStock("Bike", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odio deserunt numquam sapiente veritatis ducimus. Quaerat, beatae soluta. Rem assumenda maxime, facere vero amet corrupti cupiditate nobis in placeat nemo.", "https://i.shgcdn.com/fe14ddba-4af4-4e0f-aa6c-dfc91890cef2/-/format/auto/-/preview/3000x3000/-/quality/lighter/", 170.99);
 }
 
 function addItems(){
@@ -52,13 +52,65 @@ function addItems(){
 }
 //ViewItems
 function addCards(){
-    let cardHolder = document.querySelector(".cards");
-    let object = document.createElement("div");
-    object.classList.add("card")
+
+    let mainDisplay = document.querySelector(".cards");
+    let card;
+    let cardBody;
+    let cardHeader;
+    let cardFooter;
     
-    cardHolder.appendChild(object);
+    for (let x = 0; x < stock.length; x++){
+        // Create card
+        card = document.createElement("div");
+        // Create parts of card
+        cardHeader = document.createElement("div");
+        cardBody = document.createElement("img");
+        cardFooter = document.createElement("div")
+        // Adds cards and sub-elements
+        card.classList.add("card");
+        cardHeader.classList.add("card-header");
+        cardBody.classList.add("card-body");
+        cardFooter.classList.add("card-footer");
+    
+        cardHeader.textContent = stock[x].name;
+        cardBody.src = stock[x].picture;
+        cardFooter.textContent = "£ "+stock[x].price;
+    
+        // Adds card Items to card
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+        card.appendChild(cardFooter);
+    
+        //Adds Event Listener to card
+        card.addEventListener("click", () => { 
+            wogDisplay(stock[x].name,stock[x].description,stock[x].price,stock[x].picture);
+             })
+    
+        //Adds card to page
+        mainDisplay.appendChild(card)
+    }
 
 }
+
+
+function wogDisplay(name, desc, price, image) {
+    let mainDisplay = document.querySelector(".cards");
+    let sideDisplay = document.querySelector(".itemInset");
+    sideDisplay.style.display = "block";
+    sideDisplay.style.width = "30%";
+    mainDisplay.style.width = "70%";
+
+    document.querySelector(".itemInset .heading-1").innerHTML = name;
+    document.querySelector(".itemInset .description").innerHTML = desc;
+    document.querySelector(".itemInset .price").innerHTML = "£"+price;
+    document.querySelector(".itemInset .picture").src = image;
+    document.querySelector(".itemInset .close").addEventListener("click", () => {
+        sideDisplay.style.display = "none";
+        mainDisplay.style.width = "100%";
+    })
+    
+}
+
 
 function startup(){
     toolbarLinks();
@@ -68,6 +120,6 @@ function startup(){
     for(let x = 0;x<stock.length;x++){
         console.log(stock[x].name,stock[x].price)
     }
-    
+    addCards();
 }
 startup()
